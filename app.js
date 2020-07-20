aa54d3aaa119f39e8d53240ed90895997e091bbe
Skyes Crawford <akashiseijurou414@gmail.com>
2020-07-20 08:16:14 +0700

    menambahkan fitur edit dan delete komentar

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ var   Content            = require("./models/content"),
       Comment              = require("./models/comment");      
     //   User               = require("./models/user");      
 
-mongoose.connect("mongodb://localhost:27017/restful", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(console.log("connected to mongoDB"));
+mongoose.connect("mongodb://localhost:27017/restful", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+                ).then(() =>{
+                    console.log("connected to mongoDB")})
+                .catch((err) =>{
+                    console.log("connection error " + err)
+                })
+
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -27,11 +33,12 @@ app.use(expressSanitizer());
 //     description: "second upload content"
 // });
 
-
+//  LANDING PAGE
 app.get("/", (req, res) => {
     res.render("home");
 });
 
+//  CONTENTS PAGE
 app.get("/blogs", (req, res) => {
     Content.find({}, (err, test) => {
         if(err){
@@ -41,15 +48,15 @@ app.get("/blogs", (req, res) => {
         }
     })
 });
-
+//  ABOUT PAGE
 app.get("/about", (req, res) => {
     res.render("about");
 });
-
+//  CREATE NEW POST/FORM
 app.get("/blogs/new", (req, res) => {
     res.render("contents/new");
 });
-
+//  CREATE NEW POST/LOGIC
 app.post("/blogs", (req, res) => {
    //create blogs
    req.body.content.description = req.sanitize(req.body.content.description);
@@ -62,7 +69,7 @@ app.post("/blogs", (req, res) => {
        }
    })
 });
-
+//  SHOW/DISPLAY A CONTENT
 app.get("/blogs/:id", (req, res) => {
     Content.findById(req.params.id).populate("comments").exec((err, post) => {
         if(err){
@@ -72,7 +79,7 @@ app.get("/blogs/:id", (req, res) => {
         };
     });
 });
-
+//  EDIT/UPDATE A CONTENT/FORM
 app.get("/blogs/:id/edit", (req, res) => {
     Content.findById(req.params.id, (err, find) => {
         if(err){
@@ -82,6 +89,7 @@ app.get("/blogs/:id/edit", (req, res) => {
         };
     });
 });
+//  EDIT/UPDATE A CONTENT/LOGIC
 app.put("/blogs/:id", (req, res) => {
     req.body.edit.description = req.sanitize(req.body.edit.description);
     Content.findByIdAndUpdate(req.params.id, req.body.edit, function(err, updated){
@@ -92,7 +100,7 @@ app.put("/blogs/:id", (req, res) => {
         }
     })
 });
-
+//  DELETE/DESTROY A CONTENT
 app.delete("/blogs/:id", (req, res) => {
     Content.findByIdAndRemove(req.params.id, function(err){
         if(err){
@@ -102,6 +110,7 @@ app.delete("/blogs/:id", (req, res) => {
         }
     });
 });
+//  CREATE A NEW COMMENT/FORM
 app.get("/blogs/:id/comments/new", (req, res) => {
     Content.findById(req.params.id, (err, cont) => {
         if(err){
@@ -111,6 +120,7 @@ app.get("/blogs/:id/comments/new", (req, res) => {
         }
     })
 });
+//  CREATE A NEW COMMENT/LOGIC
 app.post("/blogs/:id/comments", (req, res) => {
     Content.findById(req.params.id, (err, content) => {
         if(err){
@@ -128,7 +138,42 @@ app.post("/blogs/:id/comments", (req, res) => {
         }
     })
 });
-
+//  UPDATE/EDIT A COMMENT/FORM
+app.get("/blogs/:id/comments/:cid/edit", (req, res) => {
+    Content.findById(req.params.id, (err, content) => {
+        if(err){
+            console.log(err)
+        } else {
+            Comment.findById(req.params.cid, (err, comment) => {
+                if(err){
+                    res.redirect("/blogs");
+                } else {
+                    res.render("comments/edit", {content: content, comment: comment});          
+                }
+            })
+        }
+    })
+});
+//  UPDATE/EDIT A COMMENT/LOGIC
+app.put("/blogs/:id/comments/:cid", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.cid, req.body.comment, (err, success) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
+//  DESTROY/DELETE A COMMENT
+app.delete("/blogs/:id/comments/:cid", (req, res) => {
+    Comment.findByIdAndRemove(req.params.cid, (err, deleted) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
 app.listen(3000, (req, res) => {
     console.log("connected...");
 })
